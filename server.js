63029aee0da81461c7512c67752d9b4955695f43
David Trowbridge <trowbrds@gmail.com>
2015-02-12 16:16:40 -0800

    Change the way static media is delivered.
    Instead of allowing express to serve any file in the current directory, specify
    the exact paths which it should serve. This required making a change in the way
    that index.html is served as well (apparently it was being shown by
    express.static instead of the route to /).
    
    Testing done:
    Verified that everything still worked.
    
    Reviewed at https://reviews.reviewboard.org/r/6938/

--- server.js
+++ server.js
@@ -2,10 +2,18 @@ var express = require('express'),
     logger = require('morgan'),
     config = require('./config'),
     routes = require('./lib/routes')(config),
-    server = express();
+    server = express(),
+    staticPaths = [
+        'lib',
+        'jspm_packages',
+        'style.css'
+    ],
+    i;
 
 server.use(logger('combined'));
-server.use(express.static('.'));
+for (i = 0; i < staticPaths.length; i++) {
+    server.use('/' + staticPaths[i], express.static(staticPaths[i]));
+}
 server.use('/', routes);
 
 server.listen(config.port, function() {
