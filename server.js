2c7ac54d501a307cd614c895516c9cb675e6d4a0
David Trowbridge <trowbrds@gmail.com>
2015-04-21 23:40:52 -0700

    Fix up additional problems in production.
    There were a few more problems with loading and compiling templates when
    running in production. This change fixes those up.

--- server.js
+++ server.js
@@ -7,29 +7,36 @@ var errorhandler = require('errorhandler'),
     init = require('./lib/init'),
     app = express();
 
-if (app.get('env') === 'production') {
-    app.use('/css', express.static('build/css'));
-    app.use('/images', express.static('build/images'));
-    app.use('/scripts', express.static('build/scripts'));
-    app.set('views', process.cwd() + '/build/views');
-} else {
-    app.use('/images', express.static('images'));
-    app.use('/lib', express.static('lib'));
-    app.use('/jspm_packages', express.static('jspm_packages'));
-    app.use('/style.css', express.static('style.css'));
-}
-
 init()
     .then(function(options) {
-        var port = options.config.port;
+        var port = options.config.port,
+            handlebarsOptions = {
+                defaultLayout: 'main',
+                compilerOptions: {}
+            },
+            viewsDir;
+
+        if (app.get('env') === 'production') {
+            app.use('/css', express.static('build/css'));
+            app.use('/images', express.static('build/images'));
+            app.use('/scripts', express.static('build/scripts'));
+
+            viewsDir = process.cwd() + '/build/views';
+            app.set('views', viewsDir);
+
+            handlebarsOptions.layoutsDir = viewsDir + '/layouts';
+            handlebarsOptions.partialsDir = viewsDir + '/partials';
+        } else {
+            app.use('/images', express.static('images'));
+            app.use('/lib', express.static('lib'));
+            app.use('/jspm_packages', express.static('jspm_packages'));
+            app.use('/style.css', express.static('style.css'));
+        }
 
         app.use(logger('combined'));
         app.use(errorhandler());
 
-        app.engine('handlebars', handlebars({
-            defaultLayout: 'main',
-            compilerOptions: {}
-        }));
+        app.engine('handlebars', handlebars(handlebarsOptions));
         app.set('view engine', 'handlebars');
 
         app.use('/', routes(options));
