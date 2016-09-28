2c5abf00af89ae4dfbb34ff84295ee2e79516e0c
David Trowbridge <trowbrds@gmail.com>
2016-09-28 13:26:53 -0700

    Switch sqs-to-dynamodb.py to the new dynamodb API
    boto has a new API for accessing dynamodb, which provides batch updates. This
    change switches us over to it.
    
    Testing done:
    Ran the script, saw that it correctly updated the dynamodb table.
    
    Reviewed at https://reviews.reviewboard.org/r/8445/

--- bin/sqs-to-dynamodb.py
+++ bin/sqs-to-dynamodb.py
@@ -2,47 +2,40 @@
 
 import json
 
-import boto.dynamodb
 import boto.sqs
+from boto.dynamodb2.table import Table
 from boto.sqs.message import RawMessage
 
 
 def main():
     sqs_cnx = boto.sqs.connect_to_region('us-east-1')
-    db_cnx = boto.dynamodb.connect_to_region('us-east-1')
-
     queue = sqs_cnx.get_queue('reviewboard-slack-logs')
     queue.set_message_class(RawMessage)
 
-    table = db_cnx.get_table('reviewboard-slack-logs')
+    table = Table('reviewboard-slack-logs')
 
     while 1:
         messages = queue.get_messages(num_messages=10, wait_time_seconds=10)
         to_delete = []
 
         if messages:
-            for message in messages:
-                body = message.get_body()
+            with table.batch_write() as batch:
+                for message in messages:
+                    body = message.get_body()
 
-                try:
-                    data = json.loads(body)
-                    attrs = {
-                        key: value
-                        for key, value in data.iteritems()
-                        if value != ''
-                    }
-                except ValueError:
-                    print '??? %r' % body
-                    continue
+                    try:
+                        data = json.loads(body)
+                        attrs = {
+                            key: value
+                            for key, value in data.iteritems()
+                            if value != ''
+                        }
 
-                item = table.new_item(
-                    hash_key=data['channel_name'],
-                    range_key=data['timestamp'],
-                    attrs=attrs)
-
-                print data['timestamp']
-                item.put()
-                to_delete.append(message)
+                        print data['timestamp']
+                        batch.put_item(attrs)
+                        to_delete.append(message)
+                    except ValueError:
+                        print '??? %r' % body
 
             sqs_cnx.delete_message_batch(queue, messages)
         else:
