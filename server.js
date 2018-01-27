22c2b7014e8389b18e4cace5263e8244a14bd52e
David Trowbridge <trowbrds@gmail.com>
2018-01-26 17:03:12 -0800

    Update server-side code to be more es6-ey
    This change changes the server-side code in Sonar to use ES6 modules and
    more idiomatic and consistent syntax. The only thing left that's still
    using `module.exports` is `lib/schema`, which manipulates the exports at
    runtime (something ES6 modules can't do). A future refactor will address
    this.
    
    As part of this, I've updated the `aws-sdk` and `memjs` dependencies to
    get newer versions which support promises. This allows us to move
    entirely over to native promises, eliminating the Q library.
    
    Testing done:
    Went through the various functionality of the server and verified that
    things work as expected.
    
    Reviewed at https://reviews.reviewboard.org/r/9541/

--- server.js
+++ server.js
@@ -1,33 +1,36 @@
 const production = (process.env.NODE_ENV === 'production');
 
-if (production) {
-    require('newrelic');
+if (!production) {
+    process.env.NEW_RELIC_ENABLED = 'false';
 }
 
 
-const bodyParser = require('body-parser'),
-      errorhandler = require('errorhandler'),
-      express = require('express'),
-      logger = require('morgan'),
-      passport = require('passport'),
-      Q = require('q'),
-      session = require('express-session'),
-      MongoStore = require('connect-mongo')(session),
-      routes = require('./lib/routes'),
-      init = require('./lib/init');
+import 'newrelic';
+import bodyParser from 'body-parser';
+import connectMongo from 'connect-mongo';
+import errorHandler from 'errorhandler';
+import express from 'express';
+import logger from 'morgan';
+import passport from 'passport';
+import session from 'express-session';
+
+import routes from './lib/routes';
+import init from './lib/init';
+
 
 init()
-    .then(function(options) {
-        let app = express();
+    .then(options => {
+        const app = express();
         app.use('/scripts', express.static('build/scripts'));
 
         if (production) {
             app.use('/images', express.static('build/images'));
         } else {
             app.use('/images', express.static('images'));
-            app.use(errorhandler());
+            app.use(errorHandler());
         }
 
+        const MongoStore = connectMongo(session);
         app.use(session({
             cookie: {
                 maxAge: 1000 * 60 * 60 * 24 * 30
@@ -50,9 +53,7 @@ init()
 
         app.use('/', routes(options));
 
-        let port = options.config.port;
-        app.listen(port, function() {
-            console.log('Listening on port %d', port);
-        });
+        const port = options.config.port;
+        app.listen(port, () => console.log('Listening on port %d', port));
     })
     .catch(console.error.bind(console));
