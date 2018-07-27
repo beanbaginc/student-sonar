1ed6b2862ced9f225b965e6da81078fd3ddaba7f
David Trowbridge <trowbrds@gmail.com>
2018-07-27 13:01:10 -0700

    Fix the project list to filter out completed projects.

--- lib/frontend/project-list.jsx
+++ lib/frontend/project-list.jsx
@@ -55,7 +55,7 @@ class ProjectList extends React.Component {
             return {
                 id: section.id,
                 name,
-                tasks: section.tasks,
+                tasks: section.tasks.filter(task => task.completed === false),
             };
         });
 
