d5551994f68894f114af1bc4226cc47717d5a0d3
David Trowbridge <trowbrds@gmail.com>
2023-06-13 09:49:34 -0600

    Update slack logs script to run on python 3

--- bin/sqs-to-dynamodb.py
+++ bin/sqs-to-dynamodb.py
@@ -2,44 +2,30 @@
 
 import json
 
-import boto.sqs
-from boto.dynamodb2.table import Table
-from boto.sqs.message import RawMessage
+import boto3
 
 
 def main():
-    sqs_cnx = boto.sqs.connect_to_region('us-east-1')
-    queue = sqs_cnx.get_queue('reviewboard-slack-logs')
-    queue.set_message_class(RawMessage)
+    dynamodb = boto3.resource('dynamodb')
+    sqs = boto3.resource('sqs')
 
-    table = Table('reviewboard-slack-logs')
+    queue = sqs.get_queue_by_name(QueueName='reviewboard-slack-logs')
+    table = dynamodb.Table('reviewboard-slack-logs')
 
-    while 1:
-        messages = queue.get_messages(num_messages=10, wait_time_seconds=10)
-        to_delete = []
+    with table.batch_writer() as batch:
+        for message in queue.receive_messages(WaitTimeSeconds=10):
+            try:
+                data = json.loads(message.body)
 
-        if messages:
-            with table.batch_write() as batch:
-                for message in messages:
-                    body = message.get_body()
+                batch.put_item(Item={
+                    key: value
+                    for key, value in data.items()
+                    if value != ''
+                })
 
-                    try:
-                        data = json.loads(body)
-                        attrs = {
-                            key: value
-                            for key, value in data.iteritems()
-                            if value != ''
-                        }
-
-                        print data['timestamp']
-                        batch.put_item(attrs)
-                        to_delete.append(message)
-                    except ValueError:
-                        print '??? %r' % body
-
-            sqs_cnx.delete_message_batch(queue, messages)
-        else:
-            break
+                message.delete()
+            except ValueError:
+                print('??? %r' % message.body)
 
 
 if __name__ == '__main__':
